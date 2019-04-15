ada237e63e84e70489278aa4cd6dc42b8bc8bfa0
aijie <413694687@qq.com>
2019-04-15 20:16:55 +0800

    修改了部分ui

--- app/src/main/java/com/example/mytest/MainActivity.java
+++ app/src/main/java/com/example/mytest/MainActivity.java
@@ -1,22 +1,11 @@
 package com.example.mytest;
 
-import android.app.Notification;
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
-import android.content.Context;
 import android.content.Intent;
-import android.databinding.DataBindingUtil;
-import android.graphics.BitmapFactory;
-import android.os.Build;
 import android.os.Bundle;
-import android.provider.Settings;
-import android.support.annotation.RequiresApi;
-import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
-import com.example.mytest.bean.UserBean;
-import com.example.mytest.databinding.ActivityMainBinding;
+import com.example.mytest.notification.NotificationTestActivity;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -24,102 +13,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ActivityMainBinding mainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
-        UserBean userBean = new UserBean("展示三", 45);
-        mainBinding.setUser(userBean);
 
-        initNotificationInfo();
-
-        findViewById(R.id.btn_chat_notification).setOnClickListener(this);
-        findViewById(R.id.btn_circle_notification).setOnClickListener(this);
-        findViewById(R.id.btn_test_donwload).setOnClickListener(this);
-
-
-    }
-
-    private void initNotificationInfo() {
-        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            String channelId = "chat";
-            String channelName = "聊天消息";
-            int importance = NotificationManager.IMPORTANCE_HIGH;
-            createNotificationChannel(channelId, channelName, importance);
-
-            String channelId1 = "friendCircle";
-            String channelName1 = "朋友圈通知";
-            int importance1 = NotificationManager.IMPORTANCE_DEFAULT;
-            createNotificationChannel(channelId1, channelName1, importance1);
-        }
-    }
-
-    @RequiresApi(api = Build.VERSION_CODES.O)
-    private void createNotificationChannel(String channelId, String channelName, int importance) {
-        NotificationChannel channel = new NotificationChannel(channelId, channelName, importance);
-        channel.setShowBadge(true);
-        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-        notificationManager.createNotificationChannel(channel);
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.btn_chat_notification:
-                sendChatMsg();
-                break;
-            case R.id.btn_circle_notification:
-                sendSubscribeMsg();
+            case R.id.btn_test_notification:
+                startActivity(new Intent(this, NotificationTestActivity.class));
                 break;
             case R.id.btn_test_donwload:
-                goDownloadTestPage();
+                startActivity(new Intent(this, DownloadApkActivity.class));
                 break;
-        }
-    }
-
-    private void goDownloadTestPage() {
-        Intent intent = new Intent(this,DownloadApkActivity.class);
-        startActivity(intent);
-    }
-
-    public void sendChatMsg() {
-        NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-        Notification notification = new NotificationCompat.Builder(this, "chat")
-                .setContentTitle("收到一条聊天消息")
-                .setContentText("今天中午吃什么？")
-                .setWhen(System.currentTimeMillis())
-                .setSmallIcon(R.mipmap.ic_launcher)
-                .setNumber(5)
-                .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher))
-                .setAutoCancel(true)
-                .build();
-        manager.notify(1, notification);
-    }
-
-    public void sendSubscribeMsg() {
-        NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-        Notification notification = new NotificationCompat.Builder(this, "friendCircle")
-                .setContentTitle("收到一条朋友圈消息")
-                .setContentText("又有好友给你点赞了，快去看看吧！")
-                .setWhen(System.currentTimeMillis())
-                .setSmallIcon(R.mipmap.ic_launcher)
-                .setNumber(5)
-                .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher))
-                .setAutoCancel(true)
-                .build();
-        manager.notify(2, notification);
-    }
-
-    public void getChannelInfo(View view) {
-        NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
-            NotificationChannel channel = manager.getNotificationChannel("chat");
-            if (channel != null) {
-                int importance = channel.getImportance();
-                if (importance == NotificationManager.IMPORTANCE_NONE) {
-                    Intent intent = new Intent(Settings.ACTION_CHANNEL_NOTIFICATION_SETTINGS);
-                    intent.putExtra(Settings.EXTRA_APP_PACKAGE, getPackageName());
-                    intent.putExtra(Settings.EXTRA_CHANNEL_ID, "chat");
-                    startActivity(intent);
-                }
-            }
         }
     }
 }
---
+++ app/src/main/java/com/example/mytest/notification/NotificationTestActivity.java
@@ -0,0 +1,107 @@
+package com.example.mytest.notification;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.NotificationCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.example.mytest.R;
+
+public class NotificationTestActivity extends AppCompatActivity implements View.OnClickListener {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notification_test);
+        initNotificationInfo();
+        findViewById(R.id.btn_chat_notification).setOnClickListener(this);
+        findViewById(R.id.btn_circle_notification).setOnClickListener(this);
+    }
+
+    private void initNotificationInfo() {
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            String channelId = "chat";
+            String channelName = "聊天消息";
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+            createNotificationChannel(channelId, channelName, importance);
+
+            String channelId1 = "friendCircle";
+            String channelName1 = "朋友圈通知";
+            int importance1 = NotificationManager.IMPORTANCE_DEFAULT;
+            createNotificationChannel(channelId1, channelName1, importance1);
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void createNotificationChannel(String channelId, String channelName, int importance) {
+        NotificationChannel channel = new NotificationChannel(channelId, channelName, importance);
+        channel.setShowBadge(true);
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.createNotificationChannel(channel);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_chat_notification:
+                sendChatMsg();
+                break;
+            case R.id.btn_circle_notification:
+                sendSubscribeMsg();
+                break;
+        }
+    }
+
+    public void sendChatMsg() {
+        NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        Notification notification = new NotificationCompat.Builder(this, "chat")
+                .setContentTitle("收到一条聊天消息")
+                .setContentText("今天中午吃什么？")
+                .setWhen(System.currentTimeMillis())
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setNumber(5)
+                .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher))
+                .setAutoCancel(true)
+                .build();
+        manager.notify(1, notification);
+    }
+
+    public void sendSubscribeMsg() {
+        NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        Notification notification = new NotificationCompat.Builder(this, "friendCircle")
+                .setContentTitle("收到一条朋友圈消息")
+                .setContentText("又有好友给你点赞了，快去看看吧！")
+                .setWhen(System.currentTimeMillis())
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setNumber(5)
+                .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher))
+                .setAutoCancel(true)
+                .build();
+        manager.notify(2, notification);
+    }
+
+    public void getChannelInfo(View view) {
+        NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            NotificationChannel channel = manager.getNotificationChannel("chat");
+            if (channel != null) {
+                int importance = channel.getImportance();
+                if (importance == NotificationManager.IMPORTANCE_NONE) {
+                    Intent intent = new Intent(Settings.ACTION_CHANNEL_NOTIFICATION_SETTINGS);
+                    intent.putExtra(Settings.EXTRA_APP_PACKAGE, getPackageName());
+                    intent.putExtra(Settings.EXTRA_CHANNEL_ID, "chat");
+                    startActivity(intent);
+                }
+            }
+        }
+    }
+}
