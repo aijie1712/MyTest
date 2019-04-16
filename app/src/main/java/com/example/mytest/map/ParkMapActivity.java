7e1c286fa3fbe98d7807bb8a7488fbda22c40fb8
aijie <413694687@qq.com>
2019-04-16 15:33:22 +0800

    添加地图多边形添加

--- app/src/main/java/com/example/mytest/map/ParkMapActivity.java
+++ app/src/main/java/com/example/mytest/map/ParkMapActivity.java
@@ -1,32 +1,43 @@
 package com.example.mytest.map;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import com.amap.api.location.AMapLocation;
 import com.amap.api.maps.AMap;
+import com.amap.api.maps.AMapUtils;
 import com.amap.api.maps.CameraUpdateFactory;
 import com.amap.api.maps.MapView;
 import com.amap.api.maps.UiSettings;
 import com.amap.api.maps.model.BitmapDescriptorFactory;
 import com.amap.api.maps.model.LatLng;
 import com.amap.api.maps.model.MyLocationStyle;
+import com.amap.api.maps.model.PolygonOptions;
 import com.example.mytest.R;
 import com.example.mytest.map.location.LocationClient;
 import com.example.mytest.map.location.MyLocationListener;
 import com.example.mytest.utils.LogUtils;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 显示停车场的地图页面
  */
-public class ParkMapActivity extends AppCompatActivity implements MyLocationListener {
+public class ParkMapActivity extends AppCompatActivity
+        implements MyLocationListener, View.OnClickListener {
     private MapView mMapView = null;
     //初始化地图控制器对象
     private AMap aMap;
 
     private double latitude;
     private double longitude;
+
+    private List<LatLng> polygonOutlineList;  // 多边形轮廓
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -39,6 +50,8 @@ public class ParkMapActivity extends AppCompatActivity implements MyLocationList
         if (aMap == null) {
             aMap = mMapView.getMap();
         }
+
+        polygonOutlineList = new ArrayList<>();
 
         new LocationClient(this).start(this, this);
     }
@@ -97,5 +110,73 @@ public class ParkMapActivity extends AppCompatActivity implements MyLocationList
         aMap.setMyLocationEnabled(true);
         // 设置定位的类型为定位模式，有定位、跟随或地图根据面向方向旋转几种
         aMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(latitude, longitude), 14f));
+        aMap.setOnMapClickListener(new AMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+                polygonOutlineList.add(latLng);
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_add_polygon:
+                addPark();
+                break;
+            case R.id.btn_remove_all:
+                aMap.clear();
+                polygonOutlineList.clear();
+                break;
+            case R.id.btn_readme:
+                startActivity(new Intent(this, MapReadmeActivity.class));
+                break;
+        }
+    }
+
+    /**
+     * 停车场--添加多边形
+     */
+    private void addPark() {
+        // 声明 多边形参数对象
+        PolygonOptions polygonOptions = new PolygonOptions();
+        // 添加 多边形的每个顶点（顺序添加）
+        polygonOptions.addAll(polygonOutlineList);
+        for (LatLng latLng : polygonOutlineList) {
+            createRectangle(latLng);
+        }
+        polygonOutlineList.clear();
+        polygonOptions.strokeWidth(5) // 多边形的边框
+                .strokeColor(getResources().getColor(R.color.map_park_stroke_color)) // 边框颜色
+                .fillColor(getResources().getColor(R.color.trans_btn_color));   // 多边形的填充色
+        // 绘制
+        aMap.addPolygon(polygonOptions);
+    }
+
+    /**
+     * 停车位-生成一个长方形的四个坐标点
+     */
+    private void createRectangle(LatLng center) {
+        double halfHeight = 1.0f / 10000;
+        double halfWidth = 0.5f / 10000;
+        List<LatLng> latLngs = new ArrayList<>();
+        LatLng leftTop = new LatLng(center.latitude - halfHeight, center.longitude - halfWidth);
+        LatLng rightTop = new LatLng(center.latitude - halfHeight, center.longitude + halfWidth);
+        LatLng rightBottom = new LatLng(center.latitude + halfHeight, center.longitude + halfWidth);
+        LatLng leftBottom = new LatLng(center.latitude + halfHeight, center.longitude - halfWidth);
+        latLngs.add(leftTop);
+        latLngs.add(rightTop);
+        latLngs.add(rightBottom);
+        latLngs.add(leftBottom);
+        float length = AMapUtils.calculateLineDistance(leftTop, rightTop);
+        float zoom = aMap.getCameraPosition().zoom;
+        LogUtils.i("aijie", "矩形的宽：" + length);
+        LogUtils.i("aijie", "真实的宽：" + length/zoom);
+        // 绘制一个长方形
+        aMap.addPolygon(new PolygonOptions()
+                .addAll(latLngs)
+                .fillColor(getResources().getColor(R.color.map_parking_space_fill_color))
+                .strokeColor(Color.RED)
+                .strokeWidth(1));
     }
 }
