b12d688751962ad65f0bab25f6613cff8dbe229f
aijie <413694687@qq.com>
2019-04-22 10:37:34 +0800

    删除部分文件

--- app/src/main/java/com/example/mytest/map/ParkMapActivity.java
+++ app/src/main/java/com/example/mytest/map/ParkMapActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
